456f7f636730b4ccf2d8cc1765ea35f6faa84a49
Tom <gts-2010@mail.ru>
2023-03-21 19:35:10 +0500

    Initial commit

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -6,14 +6,19 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.*;
 import ru.netology.repository.ProductRepository;
 
+import javax.lang.model.element.Name;
+import java.util.Arrays;
+
 public class ProductManagerTest {
     ProductRepository repository = new ProductRepository();
     ProductManager manager = new ProductManager(repository);
-    Product product1  = new Product(1, "1984", 100);
+    Product product1 = new Product(1, "1984", 100);
     Product product2 = new Product(2, "Колобок", 200);
-    Product product3 = new Product(3, "Honor", 5000);
-    Product product4  = new Book(1, "1984", 100,"Пушкин");
-    Product product5  = new Smartphone(5, "Honor", 1500,"USA");
+    Product product3 = new Product(3, "BQQ", 5000);
+    Product product4 = new Book(1, "1984", 100, "Пушкин");
+    Product product5 = new Smartphone(5, "Honor", 1500, "USA");
+    Product product6 = new Product(6, "Колобок", 300);
+
     @BeforeEach
     public void setup() {
         manager.save(product1);
@@ -21,23 +26,26 @@ public class ProductManagerTest {
         manager.save(product3);
         manager.save(product4);
         manager.save(product5);
+        manager.save(product6);
     }
 
     @Test
-    public void shouldFindByName() {
-        String name = "Колобок";
-        Product[] expected = new Product[]{product2};
+    public void shouldFindByName() {//Найти 1 продукт по названию
+        String name = "BQQ";
+        Product[] expected = new Product[]{product3};
         Product[] actual = manager.searchBy(name);
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
     @Test
     public void shouldProductDoesNotExit() {//Найти несуществующий продукт
         String nameToSearch = "ГОРОД";
         Product[] expected = {};
-        Product[] actual = manager.searchBy( nameToSearch );
-        Assertions.assertArrayEquals( expected, actual );
+        Product[] actual = manager.searchBy(nameToSearch);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldFindByAuthor() {
         String author = "Пушкин";
@@ -46,6 +54,7 @@ public class ProductManagerTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
     @Test
     public void shouldFindByManufacturer() {
         String manufacturer = "USA";
@@ -54,4 +63,24 @@ public class ProductManagerTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
+    @Test
+    public void NotOneProductDoesNotSuitable() {//Не один товар не подходит
+        String NotSuitable = "Финансист";
+        Product[] expected = {};
+        Product[] actual = manager.searchBy(NotSuitable);
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindSeveralProducts() {//Несколько товаров  с одинаковым названием (цена, id разные)
+        String Name = "Колобок";
+        Product[] expected = new Product[]{product2, product6};
+        Product[] actual = manager.searchBy(Name);
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
 }
